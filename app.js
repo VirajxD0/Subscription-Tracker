7bf4a9b67b38cc5fce3f9504d8c47fc21e8f8cde
VirajxD0 <virajvd99@gmail.com>
2025-12-14 10:55:56 +0530

    controller first commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
+import bcrypt from "bcryptjs";
+
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
